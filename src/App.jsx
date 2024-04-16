fd2ceae2cb4efb5634d38a2b7af37db9a98b1cb6
Kutay Can Kaynak <56002368+kutagorn@users.noreply.github.com>
2024-04-16 16:55:19 +0100

    Player's added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-
 function App() {
-  
-
   return (
-    <p>Coming soon...</p>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player">
+              <span className="player-name">Player 1</span>
+              <span className="player-name">X</span>
+            </span>
+            <button>Edit</button>
+          </li>
+          <li>
+            <span className="player">
+              <span className="player-name">Player 2</span>
+              <span className="player-name">O</span>
+            </span>
+          </li>
+        </ol>
+        PLAYERS
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
