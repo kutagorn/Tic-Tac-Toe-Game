2641acb1fd3c02c1233d6cf793af195920fa2f55
Kutay Can Kaynak <56002368+kutagorn@users.noreply.github.com>
2024-04-16 17:02:39 +0100

    PLayer Made in to a Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,17 @@
+import Player from "./components/Player";
+
 function App() {
   return (
     <main>
       <div id="game-container">
         <ol id="players">
-          <li>
-            <span className="player">
-              <span className="player-name">Player 1</span>
-              <span className="player-name">X</span>
-            </span>
-            <button>Edit</button>
-          </li>
-          <li>
-            <span className="player">
-              <span className="player-name">Player 2</span>
-              <span className="player-name">O</span>
-            </span>
-          </li>
+          <Player name="Player 1" symbol="X"/>
+          <Player name="Player 2" symbol="O"/>
+          
         </ol>
-        PLAYERS
+        Game Board
       </div>
+      Log
     </main>
   );
 }
