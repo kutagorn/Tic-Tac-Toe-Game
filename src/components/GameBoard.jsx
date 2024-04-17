ceb4ed3358a6587573f9875e294d7944aeb06b1e
Kutay Can Kaynak <56002368+kutagorn@users.noreply.github.com>
2024-04-17 16:46:41 +0100

    Lint

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,18 +1,23 @@
 const initalGameBoard = [
-    [null,null,null],
-    [null,null,null],
-    [null,null,null],
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
 ];
 
 export default function GameBoard() {
-    return (
-            <ol id="game-board" >
-            {initalGameBoard.map((row, rowIndex)=><li key={rowIndex}>
-                <ol>
-                    {row.map((col, colIndex) =><li key={colIndex}><button>{col} </button> </li>) }
-                </ol>
-            </li> )}
-            </ol>
-
-    )
+  return (
+    <ol id="game-board">
+      {initalGameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button>{playerSymbol} </button>{" "}
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
 }
