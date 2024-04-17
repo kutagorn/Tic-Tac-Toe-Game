407497d320f55b41b5a7f343abd33e8f9505a2c2
Kutay Can Kaynak <56002368+kutagorn@users.noreply.github.com>
2024-04-17 15:59:01 +0100

    BestPractice for updating State

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,12 +4,14 @@ export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
-    setIsEditing(true);
+    setIsEditing((editing) => !editing);
   }
-  let playerName = <span className="player-name">{name}</span>
+  let playerName = <span className="player-name">{name}</span>;
+  //let btnCaption = 'Edit';
 
   if (isEditing) {
-    playerName = (<input type="text" /> );
+    playerName = (<input type="text" required value={name} /> );
+    //btnCaption = 'Save';
   }
 
   return (
@@ -18,7 +20,7 @@ export default function Player({ name, symbol }) {
         {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'} </button>
     </li>
   );
 }
