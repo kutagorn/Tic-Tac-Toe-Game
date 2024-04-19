0533a0a7ab3c5a422c19bcbe29a0cec7c08641f0
Kutay Can Kaynak <56002368+kutagorn@users.noreply.github.com>
2024-04-19 16:15:31 +0100

    Disabling Buttons

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,22 +1,21 @@
-
 const initalGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 
-export default function GameBoard({onSelectSquare, turns } ) {
+export default function GameBoard({ onSelectSquare, turns }) {
   let gameBoard = initalGameBoard;
 
-  for(const turn of turns){
-    const {square, player} = turn;
-    const {row, col} = square;
+  for (const turn of turns) {
+    const { square, player } = turn;
+    const { row, col } = square;
     gameBoard[row][col] = player;
-//this whole thing is called deriving state
+    //this whole thing is called deriving state
   }
 
   // const [gameBoard, setGameBoard] = useState(initalGameBoard);
-  
+
   // function handleSelectSquare(rowIndex, colIndex) {
   //   setGameBoard((prevGameBoard) => {
   //     const updatedBoard = [
@@ -35,7 +34,11 @@ export default function GameBoard({onSelectSquare, turns } ) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={()=> onSelectSquare(rowIndex,colIndex) }>{playerSymbol}
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
+                  {playerSymbol}
                 </button>
               </li>
             ))}
