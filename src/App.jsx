b3f30623bccb10f8af477174925c741ac2c65492
Kutay Can Kaynak <56002368+kutagorn@users.noreply.github.com>
2024-04-19 16:12:49 +0100

    Reducing State Management & Idenftifying Unnecessary State

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,52 @@
-import {useState} from 'react';
+import { useState } from "react";
 
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
-import Log from './components/Log';
+import Log from "./components/Log";
+
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = "X";
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
+  }
+  return currentPlayer;
+  
+}
+
 
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState('X');
-
+  // const [activePlayer, setActivePlayer] = useState('X'); no need to use two states need to add some derived state
+  const activePlayer = deriveActivePlayer(gameTurns);
   function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((curActivePlayer)=>curActivePlayer ==='X' ? 'O' : 'X' );
-    setGameTurns(prevTurns=>{
-      let currentPlayer = 'X'
-      if(prevTurns.length >0 && prevTurns[0].player ==='X'){
-        currentPlayer ='O';
-      }
-      const updatedTurns = [{square: {row: rowIndex, col: colIndex} , player: currentPlayer   },
-         ...prevTurns];
-         return updatedTurns
-    } );
+    // setActivePlayer((curActivePlayer)=>curActivePlayer ==='X' ? 'O' : 'X' );
+    setGameTurns((prevTurns) => {
+      const currentPlayer = deriveActivePlayer(prevTurns);
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
+      return updatedTurns;
+    });
   }
   return (
     <main>
       <div id="game-container">
-        <ol id="players" className='highlight-player' >
-          <Player initalName="Player 1" symbol="X" isActive={activePlayer ==='X'}/>
-          <Player initalName="Player 2" symbol="O" isActive={activePlayer ==='O'}/>
+        <ol id="players" className="highlight-player">
+          <Player
+            initalName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initalName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} 
-        turns={gameTurns}
-        />
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
-      <Log turns={gameTurns}/>
+      <Log turns={gameTurns} />
     </main>
   );
 }
